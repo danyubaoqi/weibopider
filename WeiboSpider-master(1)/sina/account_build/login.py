323aea53c00292da57486abdfdff04bb82158fdc
danyubaoqi <dybq19940419>
2019-01-05 23:57:55 +0800

    1

--- WeiboSpider-master(1)/sina/account_build/login.py
+++ WeiboSpider-master(1)/sina/account_build/login.py
@@ -24,7 +24,7 @@ class WeiboLogin():
     def __init__(self, username, password):
         #os.system('pkill -f phantom')
         option=webdriver.ChromeOptions()
-        #option.add_argument("--headless")
+        option.add_argument("--headless")
         self.url = 'https://passport.weibo.cn/signin/login?entry=mweibo&r=https://weibo.cn/'
         self.browser = webdriver.Chrome(options=option)
         self.browser.set_window_size(1050, 840)
@@ -34,7 +34,6 @@ class WeiboLogin():
 
     def open(self):
         """
-        打开网页输入用户名密码并点击
         :return: None
         """
         self.browser.get(self.url)
@@ -47,8 +46,7 @@ class WeiboLogin():
 
     def get_position(self):
         """
-        获取验证码位置
-        :return: 验证码位置元组
+
         """
         try:
             img = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'patt-shadow')))
@@ -64,8 +62,7 @@ class WeiboLogin():
 
     def get_screenshot(self):
         """
-        获取网页截图
-        :return: 截图对象
+
         """
         screenshot = self.browser.get_screenshot_as_png()
         screenshot = Image.open(BytesIO(screenshot))
@@ -73,8 +70,7 @@ class WeiboLogin():
 
     def get_image(self, name='captcha.png'):
         """
-        获取验证码图片
-        :return: 图片对象
+
         """
         top, bottom, left, right = self.get_position()
         print('验证码位置', top, bottom, left, right)
@@ -84,15 +80,8 @@ class WeiboLogin():
         return captcha
 
     def is_pixel_equal(self, image1, image2, x, y):
-        """
-        判断两个像素是否相同
-        :param image1: 图片1
-        :param image2: 图片2
-        :param x: 位置x
-        :param y: 位置y
-        :return: 像素是否相同
-        """
-        # 取两个图片的像素点
+
+
         pixel1 = image1.load()[x, y]
         pixel2 = image2.load()[x, y]
         threshold = 20
@@ -103,18 +92,14 @@ class WeiboLogin():
             return False
 
     def same_image(self, image, template):
-        """
-        识别相似验证码
-        :param image: 待识别验证码
-        :param template: 模板
-        :return:
-        """
-        # 相似度阈值
+
+
+
         threshold = 0.99
         count = 0
         for x in range(image.width):
             for y in range(image.height):
-                # 判断像素是否相同
+
                 if self.is_pixel_equal(image, template, x, y):
                     count += 1
         result = float(count) / (image.width * image.height)
@@ -124,59 +109,44 @@ class WeiboLogin():
         return False
 
     def detect_image(self, image):
-        """
-        匹配图片
-        :param image: 图片
-        :return: 拖动顺序
-        """
+
         for template_name in listdir(TEMPLATES_FOLDER):
             template = Image.open(TEMPLATES_FOLDER + template_name)
             if self.same_image(image, template):
-                # 返回顺序
+
                 numbers = [int(number) for number in list(template_name.split('.')[0])]
                 print('拖动顺序', numbers)
                 return numbers
 
     def move(self, numbers):
-        """
-        根据顺序拖动
-        :param numbers:
-        :return:
-        """
-        # 获得四个按点
+
         circles = self.browser.find_elements_by_css_selector('.patt-wrap .patt-circ')
         dx = dy = 0
         for index in range(4):
             circle = circles[numbers[index] - 1]
-            # 如果是第一次循环
             if index == 0:
-                # 点击第一个按点
                 ActionChains(self.browser) \
                     .move_to_element_with_offset(circle, circle.size['width'] / 2, circle.size['height'] / 2) \
                     .click_and_hold().perform()
             else:
-                # 小幅移动次数
                 times = 30
-                # 拖动
+
                 for i in range(times):
                     ActionChains(self.browser).move_by_offset(dx / times, dy / times).perform()
                     time.sleep(1 / times)
-            # 如果是最后一次循环
+
             if index == 3:
-                # 松开鼠标
+
                 ActionChains(self.browser).release().perform()
             else:
-                # 计算下一次偏移
+
                 dx = circles[numbers[index + 1] - 1].location['x'] - circle.location['x']
                 dy = circles[numbers[index + 1] - 1].location['y'] - circle.location['y']
 
     def run(self):
-        """
-        破解入口
-        :return:
-        """
+
         self.open()
-        # 获取验证码图片
+
 
         # try:
         #     image = self.get_image('captcha.png')
@@ -195,8 +165,7 @@ class WeiboLogin():
 
 
 if __name__ == '__main__':
-    # 在目录中新建一个account.txt文件，格式需要与account_sample.txt相同
-    # 其实就是把www.xiaohao.shop买的账号复制到新建的account.txt文件中
+
     file_path =  'account.txt'
     with open(file_path, 'r') as f:
         lines = f.readlines()
